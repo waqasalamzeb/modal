c6a8ac61d0ce92961b1d8364e245c6f434355739
waqasalamzeb <79015762+waqasalamzeb@users.noreply.github.com>
2024-08-16 17:31:21 +0500

    modal

---
+++ app.js
@@ -0,0 +1,23 @@
+let container=document.querySelector(".container")
+let openbtn=document.querySelector(".openbtn")
+let closebtn=document.querySelector(".closebtn")
+let box=document.querySelector(".box")
+
+openbtn.addEventListener("click",()=>{
+    box.style.display="block"
+    openbtn.style.display="none"
+   
+}
+  
+    
+   
+  
+
+)
+closebtn.addEventListener("click",()=>{
+    box.style.display="none"
+    openbtn.style.display="block"
+   
+
+
+})
